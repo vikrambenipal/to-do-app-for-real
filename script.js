356e4bfc4fa461fc78c8a85ae50b6abb465aad1c
vikrambenipal <vikrambenipal@gmail.com>
2021-02-19 12:35:43 -0500

    drag and drop + filtering now in progress

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
 const input = document.getElementById("input");
 const form = document.getElementById("form");
 const list = document.getElementById("list");
-let x_list = document.getElementsByClassName("X");
+const x_list = document.getElementsByClassName("X");
+
+// Making the list draggable 
+// Toggle opacity on/off drag 
+const items = document.getElementsByClassName('item');
+Array.from(items).forEach(item => {
+    item.addEventListener('dragstart', () => {
+        item.classList.add('dragging');
+    })
+    item.addEventListener('dragend', () => {
+        item.classList.remove('dragging');
+    })
+})
+// Moving items within list container
+list.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(list, e.clientY);
+    const draggable = document.querySelector('.dragging');
+    if(afterElement == null) {
+        list.appendChild(draggable);
+    }else{
+        list.insertBefore(draggable, afterElement);
+    }
+})
+
+function getDragAfterElement(container, y){
+    const draggableElements = [...container.querySelectorAll('.item:not(.dragging)')];
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if(offset < 0 && offset > closest.offset) {
+             return { offset: offset, element: child }
+        }else {
+            return closest;
+        }
+    }, {offset: Number.NEGATIVE_INFINITY }).element;
+}
 
 // Remove any initial items in list 
 Array.from(x_list).forEach((x,i) => {
-    console.log(i);
     x.addEventListener('click', (e) => {
         x.parentElement.remove();
     })
 })
 
-// on form submit take input value and create a new list item 
+// On form submit take input value and create a new list item 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if(input.value != ""){
         // Define nodes in list 
         let newItem = document.createElement('div');
+        newItem.setAttribute('draggable', true);
         let task = document.createElement('p');
         task.innerText = input.value;
         let circle = document.createElement('span');
@@ -37,6 +73,14 @@ form.addEventListener('submit', (e) => {
         // Add event listener to x
         x.addEventListener('click', (e) => {
             x.parentElement.remove();
+        })
+
+        // Add draggable property to new item 
+        newItem.addEventListener('dragstart', () => {
+            newItem.classList.add('dragging');
+        })
+        newItem.addEventListener('dragend', () => {
+            newItem.classList.remove('dragging');
         })
 
         input.value = "";
